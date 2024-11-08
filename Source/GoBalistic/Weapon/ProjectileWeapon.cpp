73f3fa4632a0f2c9e4f54692aa69d63635fc4841
Devesh <deveshsoni.f5@gmail.com>
2024-11-08 18:39:03 +0530

    Projectile Tracer FX

--- Source/GoBalistic/Weapon/ProjectileWeapon.cpp
+++ Source/GoBalistic/Weapon/ProjectileWeapon.cpp
@@ -8,9 +8,10 @@
 
 void AProjectileWeapon::Fire(const FVector& HitTarget)
 {
-	APawn* InstigatorPawn = Cast<APawn>(GetOwner());
 	Super::Fire(HitTarget);
 
+	if(!HasAuthority()) return;
+	APawn* InstigatorPawn = Cast<APawn>(GetOwner());
 	const USkeletalMeshSocket* MuzzleFlashSocket = GetWeaponMesh()->GetSocketByName(FName("MuzzleFlash"));
 	if(MuzzleFlashSocket)
 	{
