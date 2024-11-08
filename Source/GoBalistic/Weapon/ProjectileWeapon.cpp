6ef8b328d53994a161b0825908f2c5748c1c085f
Devesh <deveshsoni.f5@gmail.com>
2024-11-08 17:54:24 +0530

    Spawning The Projectile

--- Source/GoBalistic/Weapon/ProjectileWeapon.cpp
+++ Source/GoBalistic/Weapon/ProjectileWeapon.cpp
@@ -3,3 +3,32 @@
 
 #include "ProjectileWeapon.h"
 
+#include "Projectile.h"
+#include "Engine/SkeletalMeshSocket.h"
+
+void AProjectileWeapon::Fire(const FVector& HitTarget)
+{
+	APawn* InstigatorPawn = Cast<APawn>(GetOwner());
+	Super::Fire(HitTarget);
+
+	const USkeletalMeshSocket* MuzzleFlashSocket = GetWeaponMesh()->GetSocketByName(FName("MuzzleFlash"));
+	if(MuzzleFlashSocket)
+	{
+		FTransform SocketTransform = MuzzleFlashSocket->GetSocketTransform(GetWeaponMesh());
+		//From Muzzle Flash Socket To Hit Location From TraceUncerCrosshairs
+		FVector ToTarget = HitTarget - SocketTransform.GetLocation();
+		FRotator TargetRotation = ToTarget.Rotation();
+		
+		if(ProjectileClass && InstigatorPawn)
+		{
+			FActorSpawnParameters SpawnParams;
+			SpawnParams.Owner = GetOwner();
+			SpawnParams.Instigator = InstigatorPawn;
+			UWorld* World = GetWorld();
+			if(World)
+			{
+				World->SpawnActor<AProjectile>(ProjectileClass, SocketTransform.GetLocation(), TargetRotation, SpawnParams );
+			}
+		}
+	}
+}
--- Source/GoBalistic/Weapon/ProjectileWeapon.h
+++ Source/GoBalistic/Weapon/ProjectileWeapon.h
@@ -13,5 +13,13 @@ UCLASS()
 class GOBALISTIC_API AProjectileWeapon : public AWeapon
 {
 	GENERATED_BODY()
+
+public:
+
+	virtual void Fire(const FVector& HitTarget) override;
 	
+private:
+
+	UPROPERTY(EditAnywhere)
+    TSubclassOf<class AProjectile> ProjectileClass;
 };
